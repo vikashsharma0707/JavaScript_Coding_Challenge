0bc389b41535e96e14c51bdba1ac51d5f7e69880
vikash sharma <vikashtiwari9893@gmail.com>
2025-07-05 17:55:42 +0530

    Reverse string

---
+++ ReverseString/index.js
@@ -0,0 +1,21 @@
+
+
+
+
+function ReverString(arr){
+    return  arr.split("").reverse().join("")
+}
+
+
+const string="Hello javascript"
+const stringreverse = ReverString(string)
+console.log(stringreverse)
+
+
+
+// 🔍 Explanation:
+// .split('') → Converts the string into an array of characters: ["h", "e", "l", "l", "o"]
+
+// .reverse() → Reverses the array: ["o", "l", "l", "e", "h"]
+
+// .join('') → Joins the array back into a string: "olleh"
