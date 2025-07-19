8ed5d70f2b170051e36475c785a4dc16629cabd3
vikash sharma <vikashtiwari9893@gmail.com>
2025-07-19 16:42:53 +0530

    sort array

---
+++ SortArray/index.js
@@ -0,0 +1,11 @@
+
+// Default Sorting (Lexicographic - for strings):
+const fruits =["apple","banana","orange","mango"]
+fruits.sort()
+
+console.log(fruits)
+
+//  Numeric Sorting (Ascending):
+const number =[1,2,4,3,5]
+number.sort()
+console.log(number)
